0acc7233d8673876d5ff95e77da19ae980149377
Kamenev Nikolay <79969997909@mail.ru>
2021-02-21 12:00:05 +0300

    Updates

---
+++ precache-manifest.326e8d88f60d1e79fa116b583dd28058.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1783167b4f6d2acc5d38fe894172405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb89324b2595d15bc28",
+    "url": "/static/css/main.49ad5582.chunk.css"
+  },
+  {
+    "revision": "5d8d620201bbdd686636",
+    "url": "/static/js/2.377252f6.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/static/js/2.377252f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db462579dcd73227b19a",
+    "url": "/static/js/3.39b3cedf.chunk.js"
+  },
+  {
+    "revision": "cfb89324b2595d15bc28",
+    "url": "/static/js/main.08392e1d.chunk.js"
+  },
+  {
+    "revision": "e3b420a424cd48935ac4",
+    "url": "/static/js/runtime-main.eb13ba12.js"
+  },
+  {
+    "revision": "692e992e486be713c701716fa8650463",
+    "url": "/static/media/dirt.692e992e.jpg"
+  },
+  {
+    "revision": "c51ca838753da76349f96567525287ba",
+    "url": "/static/media/glass.c51ca838.png"
+  },
+  {
+    "revision": "a3785d79d098aa01788e93e8e2d7b2e4",
+    "url": "/static/media/grass.a3785d79.jpg"
+  },
+  {
+    "revision": "39979b64e6478a04ff23ecc06a21c06d",
+    "url": "/static/media/wood.39979b64.png"
+  }
+]);
